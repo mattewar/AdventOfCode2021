20521f3bbb30db3333db7b836f54f580625e8b6a
Matheus Warkentin <matheus.warkentin@hotmail.com>
2021-12-28 10:04:39 -0300

    Day2Ex1

---
+++ Day2Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day2Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int depth = 0;
+            int position = 0;
+            string command = Console.ReadLine();
+            do
+            {
+                var directionParams = command.Split(' ');
+                var direction = directionParams[0];
+                var distance = int.Parse(directionParams[1]);
+
+                if (direction == "forward")
+                    position += distance;
+                if (direction == "down")
+                    depth += distance;
+                if (direction == "up")
+                    depth -= distance;
+
+                command = Console.ReadLine();
+            } while (!string.IsNullOrEmpty(command));
+            Console.WriteLine(depth * position);
+            Console.ReadLine();
+        }
+    }
+}
